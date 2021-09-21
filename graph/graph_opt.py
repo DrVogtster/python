9fcb54c697d9a7c5f02f81902282b4ddb37552ab
DrVogtster <rhvogt2@lps.umd.edu>
2021-09-21 16:12:10 -0400

    f

--- graph/graph_opt.py
+++ graph/graph_opt.py
@@ -18,6 +18,8 @@ from pyomo.environ import *
 
 dic_mn={}
 dic_m_n={}
+dic_i_j_neighbors={}
+dic_node_neighbors={}
 
 
 
@@ -45,9 +47,57 @@ def random_coeff_matrix(m,n):
             C[i,j] = random.uniform(.1, 10.0)
     return C
 
+def check_in_grid(cory,corx,m,n):
+    low_val =1
+    m_upper = m
+    n_upper = n
+
+    if( corx >=low_val and corx <= n_upper and cory>=low_val and cory <= m_upper ):
+        return True
+    else:
+        return False 
 
 
 
+def generate_neighbors_for_nodes(dic_i_j):
+
+    global dic_m_n
+    global dic_node_neighbors
+    print(dic_m_n)
+    for key in dic_i_j.keys():
+        print(key)
+        node_list=[]
+        i_j_list = dic_i_j[key]
+        
+        node_ent = dic_m_n[key]
+        print((node_ent,i_j_list))
+        for ent in i_j_list:
+            node_list.append(dic_m_n[ent])
+        dic_node_neighbors[(node_ent,)] = node_list
+        
+
+def generate_neighbors_for_graph_i_j(m,n):
+    global dic_i_j_neighbors
+  
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            tuple_list=[]
+            i_t = i
+            j_t = j
+
+        
+            north = (i_t +1,j_t)
+            east = (i_t,j_t+1)
+            south = (i_t -1,j_t)
+            west = (i_t,j_t-1)
+            dir_list = [north,east,south,west]
+            for ent in dir_list:
+                if(check_in_grid(ent[0],ent[1],m,n)):
+                    tuple_list.append(ent)
+            dic_i_j_neighbors[(i_t,j_t)]= tuple_list
+  
+
+    return dic_i_j_neighbors
 
 
 
@@ -116,23 +166,26 @@ def V_no_overlap(M,n, i,j):
     return sum(M.V[n,k,i,j]+M.V[n,k,j,i]  for k in M.k)<=2
 def V_paths(M,n,k,i):
     global dic_mn
+    global dic_node_neighbors
     # M.X = Var(M.n_steps,M.k,M.nn,M.nm, domain=Binary)
-    res = get_dic_mn_ele(dic_mn,str(i))
-    res2 = res.split(',')
-    for v in range(0,len(res2)):
-        res2[v] = int(int(res2[v])) 
+    res2 = get_dic_mn_ele(dic_mn,(i,))
+    node_list = dic_node_neighbors[(i,)]
+    # for v in range(0,len(res2)):
+    #     res2[v] = int(int(res2[v])) 
     return sum(M.V[n,k,i,j] +M.V[n,k,j,i]for j in M.mn) == sum(M.X[n,k,p,res2[0],res2[1]]*M.L[n,k,p] for p in M.nn)
 
 def X_place_nodes(M,n, k,nn):
     return sum(M.X[n,k,nn,i,j]  for i in M.m for j in M.n)== M.L[n,k,nn]*M.L[n,k,nn]
+def X_place_nodes_no_double_placement(M,n,k,i,j):
+    return sum(M.X[n,k,nn,i,j]  for nn in M.nn)<=1
 def generate_mn_m_n_dics(m,n):
     global dic_mn
     global dic_m_n
     counter=1
     for i in range(1,m+1):
         for j in range(1,n+1):
-            dic_mn[str(counter)] = str(i)+","+str(j)
-            dic_m_n[str(i)+","+str(j)] = str(counter)
+            dic_mn[(counter,)] = (i,j)
+            dic_m_n[(i,j)] = counter
             counter+=1
     return dic_m_n,dic_mn
 
@@ -164,8 +217,11 @@ def graph_opt_fun(m,n,k,n_steps,number_nodes,file):
     generate_mn_m_n_dics(m,n)
     C = random_coeff_matrix(m*n,m*n)
     L =loc_matrix(n_steps,k,number_nodes,seq_list)
+    print(L)
     L =mat_func3d(L)
 
+    my_dic_i_j = generate_neighbors_for_graph_i_j(m,n)
+    generate_neighbors_for_nodes(my_dic_i_j)
     py_c = mat_func(C)
     M = AbstractModel()
     M.n = RangeSet(1,n)
@@ -183,10 +239,15 @@ def graph_opt_fun(m,n,k,n_steps,number_nodes,file):
 
     M.C1 = Constraint(M.n_steps,M.k,M.nn, rule=X_place_nodes)
     M.C2 = Constraint(M.n_steps,M.k,M.mn, rule=V_paths)
+    M.C3 =Constraint(M.n_steps,M.k,M.m,M.n,rule=X_place_nodes_no_double_placement)
 
     M.obj = Objective(rule=J, sense=minimize)
+    #M.C2.pprint()
+  
 
     instance = M.create_instance()
+    instance.pprint()
+   
     # results=SolverFactory('mindtpy').solve(instance,strategy='OA',
     #                             time_limit=3600, mip_solver='glpk', nlp_solver='ipopt',tee=True)
     opt = SolverFactory("glpk")
@@ -201,18 +262,24 @@ def graph_opt_fun(m,n,k,n_steps,number_nodes,file):
     #        print(instance.V[n,k,:,:].value)
     #     print("-----------")
     
-    print(sum(instance.X[1,2,3,i,j].value for i in M.m for j in M.n ))
+    # print(instance.X[1,1,1,1,1].value)
+    # print(instance.X[1,1,2,1,1].value)
+    # print(instance.X[1,1,1,1,2].value)
+    # print(instance.X[1,1,2,1,2].value)
+    #print(sum(instance.X[1,1,k,i,j].value for k in M.nn for i in M.m for j in M.n ))
+    #print(sum(instance.V[1,2,i,j].value for i in M.m for j in M.n ))
 
-
-out=parser("seq.txt")
+out=parser("seq2.txt")
 # print(random_coeff_matrix(5,5))
 
-file_name="seq.txt"
-m=5
-n=3
-k=2
-n_steps=3
-number_nodes=4
+file_name="seq2.txt"
+m=2
+n=2
+k=1
+n_steps=1
+number_nodes=2
+
+
 
 graph_opt_fun(m,n,k,n_steps,number_nodes,file_name)
 #print(loc_matrix(3,4,out))
